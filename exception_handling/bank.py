2e5c2ab04490249a131db0aaf83bff18d0aa70ff
Kristen Norris <krnorris65@gmail.com>
2020-04-06 18:51:35 -0500

    updated exception exercise

--- exception_handling/bank.py
+++ exception_handling/bank.py
@@ -14,7 +14,7 @@ class BankAccount():
             return self.balance
         except TypeError:
             print('(Error): The add_money method requires a numeric value')
-            raise
+            
 
     def withdraw_money(self, amount):
         """Withdraw money to a bank account
@@ -23,16 +23,20 @@ class BankAccount():
           amount - A numerical value by which the bank account's balance will decrease
         """
         try:
-            self.balance -= amount
-            return self.balance
+            if amount < self.balance:
+                self.balance -= amount
+                return self.balance
+            else:
+                print(f"You cannot withdraw ${amount}. You only have ${self.balance} in your account")
         except TypeError:
             print('(Error): The withdraw_money method requires a numeric value')
-            raise
+            
 
 
 bank = BankAccount()
 
 bank.add_money(50)
-bank.withdraw_money("40")
+bank.withdraw_money(60)
+bank.withdraw_money("60")
 
 print(bank.balance)
