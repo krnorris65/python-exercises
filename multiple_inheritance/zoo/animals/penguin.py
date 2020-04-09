4745d2f67fd9fb50f484c39439b6f490cafdabd9
Kristen Norris <krnorris65@gmail.com>
2020-04-09 12:07:45 -0500

    completed chapter walk through of zoo

---
+++ multiple_inheritance/zoo/animals/penguin.py
@@ -0,0 +1,15 @@
+from movements import IWalking, ISwimming
+
+
+class Penguin(IWalking, ISwimming):
+
+    def __init__(self, name):
+        ISwimming.__init__(self)
+        IWalking.__init__(self)
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name} the Penguin'
+
+    def run(self):
+        print(f'{self} waddles')
